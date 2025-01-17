64b37420f081806079cdecb77bc244aa8e87c34c
ulyan <ulyanafisa0@gmail.com>
2025-01-17 22:10:15 +0700

    database dokter

---
+++ database/migrations/2025_01_17_143014_create_dokters_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('dokters', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('kode_dokter');
+            $table->string('nama_dokter');
+            $table->text('kampus');
+            $table->string('foto')->nullable();
+            $table->string('spesialis');
+            $table->string('nomor_hp');
+            $table->string('twitter')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('tiktok')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('dokters');
+    }
+};
